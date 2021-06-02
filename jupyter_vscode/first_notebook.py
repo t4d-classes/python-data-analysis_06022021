1ef33154d3746ce0965ad3775ec5798f0835f3e0
Eric Greene <eric@t4d.io>
2021-06-02 15:46:08 -0400

    create jupyter environments, explored numpy

---
+++ jupyter_vscode/first_notebook.py
@@ -0,0 +1,48 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %% [markdown]
+# # NumPy Demos
+# 
+# Here are some NumPy demos!
+
+# %%
+nums = list(range(20))
+
+# nums
+# print(nums)
+display(nums)
+
+
+# %%
+print("some output")
+print("some more output")
+
+display("some output")
+display("some more output")
+
+# output the final expression
+# "some output"
+# "some more output"
+
+
+# %%
+double_nums = [ num * 2 for num in nums ]
+
+
+# %%
+display(double_nums)
+
+
+# %%
+a = 2
+b = 4
+
+c = a + b
+
+display(c)
+
+
+# %%
+
+
+
